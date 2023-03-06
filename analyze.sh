6d67a2437bc1e955744a151754bdddaf1bb1a1d8
AkashiSensei <fengzhiyuyi2013@gmail.com>
2023-03-06 19:42:46 +0800

    first try

--- analyze.sh
+++ analyze.sh
@@ -3,19 +3,29 @@
 if [ $# -eq 1 ];
 then
     # Your code here. (1/4)
+    grep -E "WARN|ERROR" $1 >bug.txt
 
 else
     case $2 in
     "--latest")
         # Your code here. (2/4)
+	tail -n 5 $1
 
     ;;
     "--find")
         # Your code here. (3/4)
+	grep $3 >${3}.txt
 
     ;;
     "--diff")
         # Your code here. (4/4)
+	diff --brief $1 $3 >/dev/null
+	if [ $? -eq 0 ];then
+		echo same
+	else
+		echo different
+		fi
+
 
     ;;
     esac
