93e291fdadf559b0680c4370b548173e954dae15
刘益洲 <20374249@buaa.edu.cn>
2023-03-14 14:03:45 +0800

    first try for 2_1

--- kern/pmap.c
+++ kern/pmap.c
@@ -25,6 +25,7 @@ void mips_detect_memory() {
 
 	/* Step 2: Calculate the corresponding 'npage' value. */
 	/* Exercise 2.1: Your code here. */
+	npage = memsize / BY2PG;
 
 	printk("Memory size: %lu KiB, number of pages: %lu\n", memsize / 1024, npage);
 }
@@ -90,15 +91,27 @@ void page_init(void) {
 	/* Step 1: Initialize page_free_list. */
 	/* Hint: Use macro `LIST_INIT` defined in include/queue.h. */
 	/* Exercise 2.3: Your code here. (1/4) */
+	LIST_INIT(&page_free_list);
 
 	/* Step 2: Align `freemem` up to multiple of BY2PG. */
 	/* Exercise 2.3: Your code here. (2/4) */
+	freemem = ROUND(freemem, BY2PG);
 
 	/* Step 3: Mark all memory below `freemem` as used (set `pp_ref` to 1) */
 	/* Exercise 2.3: Your code here. (3/4) */
+	extern char end[];
+	int index = (freemem - (u_long)end) / BY2PG;
+	while(index--) {
+		pages[index].pp_ref = 1;
+	}
 
 	/* Step 4: Mark the other memory as free. */
 	/* Exercise 2.3: Your code here. (4/4) */
+	int lowest_empty = (freemem - (u_long)end) / BY2PG;
+	for (index = npage; index >= lowest_empty; index--) {
+		pages[index].pp_ref = 0;
+		LIST_INSERT_HEAD(&page_free_list, &pages[index], pp_link);
+	}
 
 }
 
@@ -119,12 +132,17 @@ int page_alloc(struct Page **new) {
 	/* Step 1: Get a page from free memory. If fails, return the error code.*/
 	struct Page *pp;
 	/* Exercise 2.4: Your code here. (1/2) */
+	if (LIST_EMPTY(&page_free_list)) {
+		return -E_NO_MEM;
+	}
 
+	pp = LIST_FIRST(&page_free_list);
 	LIST_REMOVE(pp, pp_link);
 
 	/* Step 2: Initialize this page with zero.
 	 * Hint: use `memset`. */
 	/* Exercise 2.4: Your code here. (2/2) */
+	memset((void *)page2kva(pp), 0, BY2PG);
 
 	*new = pp;
 	return 0;
@@ -140,6 +158,7 @@ void page_free(struct Page *pp) {
 	assert(pp->pp_ref == 0);
 	/* Just insert it into 'page_free_list'. */
 	/* Exercise 2.5: Your code here. */
+	LIST_INSERT_HEAD(&page_free_list, pp, pp_link);
 
 }
 
@@ -353,9 +372,14 @@ void physical_memory_manage_check(void) {
 	while (p != NULL) {
 		// printk("%d %d\n",p->pp_ref,answer1[j]);
 		assert(p->pp_ref == answer1[j++]);
-		// printk("ptr: 0x%x v: %d\n",(p->pp_link).le_next,((p->pp_link).le_next)->pp_ref);
+		// if(j == 10)printk("in\n");
+		// printk("ptr: 0x%x\n", (p->pp_link).le_next);
+		// printk("v: %d\n", ((p->pp_link).le_next)->pp_ref);
+		// printk("ptr: 0x%x v: %d\n\n\n",(p->pp_link).le_next,((p->pp_link).le_next)->pp_ref);
+		//if(j == 10)printk("out\n");
 		p = LIST_NEXT(p, pp_link);
 	}
+	// printk("111");
 	// insert_after test
 	int answer2[] = {0, 1, 2, 3, 4, 20, 5, 6, 7, 8, 9};
 	q = (struct Page *)alloc(sizeof(struct Page), BY2PG, 1);
@@ -363,6 +387,7 @@ void physical_memory_manage_check(void) {
 
 	// printk("---%d\n",test_pages[4].pp_ref);
 	LIST_INSERT_AFTER(&test_pages[4], q, pp_link);
+	// printk("222");
 	// printk("---%d\n",LIST_NEXT(&test_pages[4],pp_link)->pp_ref);
 	p = LIST_FIRST(&test_free);
 	j = 0;
