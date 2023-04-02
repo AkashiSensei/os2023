65f91be37011e2b6df9a96d30b0a1ce01265f137
刘益洲 <20374249@buaa.edu.cn>
2023-04-02 16:54:44 +0800

    modify sched.c

--- kern/sched.c
+++ kern/sched.c
@@ -38,9 +38,13 @@ void schedule(int yield) {
 	count--;
 
 	if(yield || count <= 0 || e == NULL || e->env_status != ENV_RUNNABLE) {
-		if(e != NULL && e->env_status == ENV_RUNNABLE) {
-			TAILQ_REMOVE(&env_sched_list, e, env_sched_link);
-			TAILQ_INSERT_TAIL(&env_sched_list, e, env_sched_link);
+		if(e != NULL) {
+			if(e == TAILQ_FIRST(&env_sched_list)) {
+				TAILQ_REMOVE(&env_sched_list, e, env_sched_link);
+			}
+			if(e->env_status == ENV_RUNNABLE) {
+				TAILQ_INSERT_TAIL(&env_sched_list, e, env_sched_link);
+			}
 		}
 		if(TAILQ_EMPTY(&env_sched_list)) {
 			panic("schedule: no runnable envs\n");
