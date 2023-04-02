3b656a98c88bf841f9279e32acfad143b564ed9f
刘益洲 <20374249@buaa.edu.cn>
2023-04-02 15:06:13 +0800

    1st try for 3_34

--- kern/sched.c
+++ kern/sched.c
@@ -35,5 +35,18 @@ void schedule(int yield) {
 	 *   'TAILQ_FIRST', 'TAILQ_REMOVE', 'TAILQ_INSERT_TAIL'
 	 */
 	/* Exercise 3.12: Your code here. */
+	count--;
+
+	if(yield || count <= 0 || e == NULL || e->env_status != ENV_RUNNABLE) {
+		if(e != NULL && e->env_status == ENV_RUNNABLE) {
+			TAILQ_REMOVE(&env_sched_list, e, env_sched_link);
+			TAILQ_INSERT_TAIL(&env_sched_list, e, env_sched_link);
+		}
+		if(TAILQ_EMPTY(&env_sched_list)) {
+			panic("schedule: no runnable envs\n");
+		}
+		e = TAILQ_FIRST(&env_sched_list);
+	}
+	env_run(e);
 
 }
