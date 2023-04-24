f57953b4e9bbb86ee01686f5067fb0550d9caa37
AkashiSensei <fengzhiyuyi2013@gmail.com>
2023-04-24 20:09:50 +0800

    1st try for 4-1-extra

--- user/lib/ipc.c
+++ user/lib/ipc.c
@@ -16,7 +16,32 @@ void ipc_send(u_int whom, u_int val, const void *srcva, u_int perm) {
 	}
 	user_assert(r == 0);
 }
+void ipc_broadcast(u_int val, void * srcva, u_int perm) {
+	u_int id;
+	id = syscall_getenvid();
+	ipc_broadcast_send(id, val, srcva, perm);
+}
 
+void ipc_broadcast_send(u_int p_envid, u_int val, void * srcva, u_int perm) {
+	int num = 0;
+	struct Env *child_envs[10];
+	int i;
+
+	for(i = 0; i < NENV; i++) {
+		if(envs[i].env_status != ENV_FREE && env[i].env_parent_id == p_envid) {
+			child_envs[num++] = &envs[i];
+		}
+	}
+
+	if(num == 0) {
+		return;
+	}
+
+	for(i = 0; i < num; i++) {
+		ipc_send(child_envs[i]->env_id, val, srcva, perm);
+		ipc_broadcast_send(child_envs[i]->env_id, val, srcva, perm);
+	}
+}
 // Receive a value.  Return the value and store the caller's envid
 // in *whom.
 //
@@ -37,3 +62,4 @@ u_int ipc_recv(u_int *whom, void *dstva, u_int *perm) {
 
 	return env->env_ipc_value;
 }
+
